943eea5bd431f1c2909998034af766b27f7da98a
Josh Merritt <merrittj@aston.ac.uk>
2017-04-07 08:39:10 +0100

    Settings and results halfway

--- src/Model/main.java
+++ src/Model/main.java
@@ -10,20 +10,35 @@ public class main {
 		try {
 			int runs = 5;
 			ArrayList<String> settings = new ArrayList<String>();
-			settings.add("Settings/settings1.properties");
-			settings.add("Settings/settings2.properties");
-			settings.add("Settings/settings3.properties");
-//			settings.add("Settings/settings4.properties");
-//			settings.add("Settings/settings5.properties");
 
-			ArrayList<ArrayList<ArrayList<Result<Integer, Double, Integer, Integer>>>> results = new ArrayList<ArrayList<ArrayList<Result<Integer, Double, Integer, Integer>>>>(); //run -> gen -> pop#,fit,turb#
-			
+			//settings.add("settings11");
+//			settings.add("settings12");
+			//settings.add("settings13");
+//			settings.add("settings14");
+			//settings.add("settings15");
+			settings.add("settings21");
+//			settings.add("settings22");
+			settings.add("settings23");
+//			settings.add("settings24");
+			settings.add("settings25");
+			//settings.add("settings31");
+//			settings.add("settings32");
+			//settings.add("settings33");
+//			settings.add("settings34");
+			//settings.add("settings35");
+			//settings.add("settings41");
+//			settings.add("settings42");
+			//settings.add("settings43");
+//			settings.add("settings44");
+			//settings.add("settings45");
+
 			for (int j = 0;j<settings.size();j++){
+				ArrayList<ArrayList<ArrayList<Result<Integer, Double, Integer, Integer>>>> results = new ArrayList<ArrayList<ArrayList<Result<Integer, Double, Integer, Integer>>>>(); //run -> gen -> pop#,fit,turb#
 				for (int i = 0; i<runs;i++){
 					WindScenario ws = new WindScenario("Scenarios/00.xml"); //def = Scenarios/obs_00.xml
 					KusiakLayoutEvaluator wfle = new KusiakLayoutEvaluator();
 					wfle.initialize(ws);
-					MyGA2 myga = new MyGA2(wfle, settings.get(j), i);
+					MyGA2 myga = new MyGA2(wfle, "Settings/"+settings.get(j)+".properties", i);
 					//View v = new View(myga.getBestLayout());
 
 					results.add(myga.getResults());
@@ -37,7 +52,7 @@ public class main {
 
 				ExcelWriter test = new ExcelWriter();
 
-				test.setOutputFile("Results/settings"+j+"Results.xls");
+				test.setOutputFile("Results/"+settings.get(j)+"Results.xls");
 				test.setResults(results);
 				test.write();
 				System.out.println("results written");
